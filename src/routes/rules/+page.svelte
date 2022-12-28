1820aa89629cd99251bf940c10a7a1f915f34257
fcrozatier <frederic.crozatier@protonmail.com>
2022-12-28 17:44:37 +0100

    create rules section

---
+++ src/routes/rules/+page.svelte
@@ -0,0 +1,96 @@
+<script>
+	import { PUBLIC_REGISTRATION_DEADLINE } from '$env/static/public';
+	import { getContext } from 'svelte';
+
+	const intl = getContext('intl');
+</script>
+
+<article>
+	<h2>What are the constraints for entries?</h2>
+
+	<ol>
+		<li>It has to be something new you make this summer</li>
+		<p>
+			The spirit of this is to encourage people who've never put stuff online before. If you want to
+			work on something you sort of started once before, that's probably fine, but it can't be
+			something you already published before this contest. Optimally, you'd use this as a chance to
+			try something new you otherwise might not have.
+		</p>
+
+		<li>It has to be about math.</li>
+		<p>
+			Here we mean "math" very broadly, and more applied topics like physics or computer science are
+			abundantly welcome. It just has to be the case that a viewer/reader might come away knowing
+			something mathematical they didn't before.
+		</p>
+		<p>
+			The topic could be at any level, whether that's basic math for young children or higher-level
+			math. If you're assuming a certain background level for the target audience, kindly mention it
+			below. It's hard because we don't want to discourage topics with a very niche target audience,
+			as those lessons can sometimes be the most valuable. However, if your lesson assumes
+			particular expertise, e.g. a comfort with algebraic geometry, keep in mind that our judges may
+			not fit into this category. So to actually win the contest, it's helpful if the topic is
+			accessible to someone with, say, a background in standard undergrad math topics.
+		</p>
+
+		<li>One entry per person/group</li>
+		<p>
+			We hope you make more, but we only have the capacity to judge participants based on a single
+			entry.
+		</p>
+		<li>It has to be available in English</li>
+		<p>
+			If you want to put out an explainer in another language, wonderful! Please do! But the judges
+			here will be english speakers, so to be considered for the contest the lesson has to be
+			accessible to them.
+		</p>
+		<li>It has to be publicly visible and available for people to consume for free</li>
+	</ol>
+
+	<h2>When is the deadline?</h2>
+	<p>
+		<time datetime={PUBLIC_REGISTRATION_DEADLINE}
+			>{intl.format(Date.parse(PUBLIC_REGISTRATION_DEADLINE))}</time
+		>
+	</p>
+
+	<h2>How will winners be selected?</h2>
+	<p>Here's what we're looking for:</p>
+	<ul>
+		<li>
+			<strong> Clarity: </strong>
+			Jargon should be explained, the goals of the lesson should be understandable with minimal background,
+			and the submission should generally display empathy for people unfamiliar with the topic.
+		</li>
+		<li>
+			<strong>Motivation:</strong> It should be clear to the reader/viewer within the first 30 seconds
+			why they should care.
+		</li>
+		<li>
+			<strong>Novelty:</strong> It doesn't necessarily have to be an original idea or original topic,
+			but it should offer someone an experience they might otherwise not have by searching around online.
+			Some of the greatest value comes from covering common topics in better ways. Other times there's
+			value in surfacing otherwise obscure ideas which more people should know about.
+		</li>
+		<li>
+			<strong>Memorable:</strong> Something should make the piece easy to remember even several months
+			later. Maybe it's the beauty of the presentation, the enthusiasm of the presenter, or the mind-blowingness
+			of an aha moment.
+		</li>
+	</ul>
+	<p>
+		To select the final winners, judges will look through the entries with the criteria above in
+		mind. Depending on the number of entries, we may have a step to pre-filter entries before these
+		final judgments. In all likelihood, to be considered for the competition, we may ask you to peer
+		review some of the other participants' submissions. This would involve looking at five other
+		entries and ranking them 1-5 based on how well they fit the criteria above, at which point we'll
+		use the cumulative scoring which comes from those rankings to determine a final set of a few
+		hundred which is manageable for our judges.
+	</p>
+	<p>
+		For longer works, judges might not be able to consume the full video/post. Again, what's hard
+		about this is that sometimes great explainers are longer, such as a full lecture and we don't
+		want to discourage those. Just understand that to select winners, there is only so much time, so
+		the substance of your work should be clearly visible with a 5-10 minute view.
+	</p>
+</article>
