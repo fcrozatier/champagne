d0d445b3e75a670bccac52b3ac9befda6cc8acbb
fcrozatier <frederic.crozatier@protonmail.com>
2022-12-28 14:10:23 +0100

    add tailwind and daisyui

---
+++ tailwind.config.cjs
@@ -0,0 +1,9 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+	mode: 'jit',
+	content: ['./src/**/*.{html,js,svelte,ts}'],
+	theme: {
+		extend: {}
+	},
+	plugins: [require('daisyui')]
+};
