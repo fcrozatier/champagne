ac393e0dc9f973a833e8fb5d2beb037d7e2f46e0
fcrozatier <frederic.crozatier@protonmail.com>
2022-12-28 17:44:12 +0100

    share intl in context

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,5 +1,18 @@
 <script>
 	import '../app.css';
+	import { setContext } from 'svelte';
+
+	setContext(
+		'intl',
+		new Intl.DateTimeFormat('en', {
+			// weekday: 'long',
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+			hour: 'numeric',
+			minute: 'numeric'
+		})
+	);
 </script>
 
 <main class="prose mx-auto mt-20 mb-40">
