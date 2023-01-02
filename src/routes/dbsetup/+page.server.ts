7e19616013b5bdc4355acbcd1dcdc984ac1a9526
fcrozatier <frederic.crozatier@protonmail.com>
2023-01-02 17:20:25 +0100

    adds background job

--- src/routes/dbsetup/+page.server.ts
+++ src/routes/dbsetup/+page.server.ts
@@ -6,47 +6,58 @@ export const load: PageServerLoad = async () => {
 	try {
 		await session.executeWrite((tx) => {
 			return tx.run(`
-      CREATE CONSTRAINT UserEmailUnique
-      IF NOT EXISTS
-      FOR (user:User)
-      REQUIRE user.email IS UNIQUE;
+				CREATE CONSTRAINT UserEmailUnique
+				IF NOT EXISTS
+				FOR (user:User)
+				REQUIRE user.email IS UNIQUE;
       `);
 		});
 
 		await session.executeWrite((tx) => {
 			return tx.run(`
-      CREATE CONSTRAINT EntryLinkUnique
-      IF NOT EXISTS
-      FOR (entry:Entry)
-      REQUIRE entry.link IS UNIQUE;
+				CREATE CONSTRAINT EntryLinkUnique
+				IF NOT EXISTS
+				FOR (entry:Entry)
+				REQUIRE entry.link IS UNIQUE;
       `);
 		});
 
 		// Indexes
 		await session.executeWrite((tx) => {
 			return tx.run(`
-      CREATE INDEX UserTokenIndex
-      IF NOT EXISTS
-      FOR (user:User)
-      ON user.token;
+				CREATE INDEX UserTokenIndex
+				IF NOT EXISTS
+				FOR (user:User)
+				ON user.token;
       `);
 		});
 
 		await session.executeWrite((tx) => {
 			return tx.run(`
-      CREATE INDEX EntryNumberIndex
-      IF NOT EXISTS
-      FOR (entry:Entry)
-      ON entry.number;
+				CREATE INDEX EntryNumberIndex
+				IF NOT EXISTS
+				FOR (entry:Entry)
+				ON entry.number;
       `);
 		});
 
 		// Sequence for entries
 		await session.executeWrite((tx) => {
 			return tx.run(`
-      MERGE (s:Seq)
-      ON CREATE
-        SET s.value = 0
+				MERGE (s:Seq)
+				ON CREATE
+					SET s.value = 0
+      `);
+		});
+
+		// Background job: periodically (hourly) remove stale assigned comparisons (more than 24h old)
+		await session.executeWrite((tx) => {
+			return tx.run(`
+				CALL apoc.periodic.repeat(
+				"remove-stale-assignements",
+					"MATCH ()-[r:ASSIGNED]->() WHERE r.timestamp >= timestamp() - 1000 * 60 * 30 DELETE r",
+					60 * 60
+				);
       `);
 		});
 
