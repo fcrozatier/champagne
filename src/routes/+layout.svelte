9014c56067bc1e3c23a668ade3d7cc3461502181
fcrozatier <frederic.crozatier@protonmail.com>
2023-01-12 17:28:33 +0100

    add footer

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -10,7 +10,9 @@
 	<div class="flex-1 " />
 </header>
 
-<main class="grid justify-center prose max-w-full mt-10 mb-40">
-	<h1 class="text-center">Champagne x SOME 2.5</h1>
+<main class="prose max-w-full mt-10 mb-40">
+	<h1 class="text-center">SOME 2.5</h1>
 	<slot />
 </main>
+
+<footer>Champagne</footer>
