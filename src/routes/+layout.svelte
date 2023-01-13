79cb84214b34b359c747179ae30663ff92610903
fcrozatier <frederic.crozatier@protonmail.com>
2023-01-13 17:56:52 +0100

    add footer link

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -2,17 +2,21 @@
 	import '../app.css';
 </script>
 
-<header class="navbar px-4">
-	<nav class="">
-		<a href="/">Home</a>
-	</nav>
+<div class="h-full flex flex-col">
+	<header class="navbar px-4">
+		<nav class="">
+			<a href="/">Home</a>
+		</nav>
 
-	<div class="flex-1 " />
-</header>
+		<div class="flex-1 " />
+	</header>
 
-<main class="prose max-w-full mt-10 mb-40">
-	<h1 class="text-center">SOME 2.5</h1>
-	<slot />
-</main>
+	<main class="prose max-w-full mt-10 mb-40">
+		<h1 class="text-center">SOME 2.5</h1>
+		<slot />
+	</main>
 
-<footer>Champagne</footer>
+	<footer class="mt-auto">
+		<a href="https://github.com/fcrozatier/champagne"> Champagne </a>
+	</footer>
+</div>
