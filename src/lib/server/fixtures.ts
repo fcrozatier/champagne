a7610f48e842f0c8faa84879e8876dd177c28be0
fcrozatier <frederic.crozatier@protonmail.com>
2023-01-23 12:02:02 +0100

    add number property to fixture

--- src/lib/server/fixtures.ts
+++ src/lib/server/fixtures.ts
@@ -3,147 +3,90 @@ export const creatorsData = [
 		email: 'email1@gmail.com',
 		entry: 'video',
 		title: 'title1',
-		description: 'description1',
-		link: 'http://link1.com'
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem sequi ipsum ex dolores, placeat dolor blanditiis est voluptatibus similique ad nobis ab fugit explicabo tempore asperiores aliquid id itaque neque.',
+		link: 'http://link1.com',
+		number: 0
 	},
 	{
 		email: 'email2@gmail.com',
 		entry: 'video',
 		title: 'title2',
-		description: 'description2',
-		link: 'http://link2.com'
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem sequi ipsum ex dolores, placeat dolor blanditiis est voluptatibus similique ad nobis ab fugit explicabo tempore asperiores aliquid id itaque neque.',
+		link: 'http://link2.com',
+		number: 1
 	},
 	{
 		email: 'email3@gmail.com',
 		entry: 'video',
 		title: 'title3',
-		description: 'description3',
-		link: 'http://link3.com'
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem sequi ipsum ex dolores, placeat dolor blanditiis est voluptatibus similique ad nobis ab fugit explicabo tempore asperiores aliquid id itaque neque.',
+		link: 'http://link3.com',
+		number: 2
 	},
 	{
 		email: 'email4@gmail.com',
 		entry: 'video',
 		title: 'title4',
-		description: 'description4',
-		link: 'http://link4.com'
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem sequi ipsum ex dolores, placeat dolor blanditiis est voluptatibus similique ad nobis ab fugit explicabo tempore asperiores aliquid id itaque neque.',
+		link: 'http://link4.com',
+		number: 3
 	},
 	{
 		email: 'email5@gmail.com',
 		entry: 'video',
 		title: 'title5',
-		description: 'description5',
-		link: 'http://link5.com'
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem sequi ipsum ex dolores, placeat dolor blanditiis est voluptatibus similique ad nobis ab fugit explicabo tempore asperiores aliquid id itaque neque.',
+		link: 'http://link5.com',
+		number: 4
 	},
 	{
 		email: 'email6@gmail.com',
 		entry: 'video',
 		title: 'title6',
-		description: 'description6',
-		link: 'http://link6.com'
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem sequi ipsum ex dolores, placeat dolor blanditiis est voluptatibus similique ad nobis ab fugit explicabo tempore asperiores aliquid id itaque neque.',
+		link: 'http://link6.com',
+		number: 5
 	},
 	{
 		email: 'email7@gmail.com',
 		entry: 'video',
 		title: 'title7',
-		description: 'description7',
-		link: 'http://link7.com'
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem sequi ipsum ex dolores, placeat dolor blanditiis est voluptatibus similique ad nobis ab fugit explicabo tempore asperiores aliquid id itaque neque.',
+		link: 'http://link7.com',
+		number: 6
 	},
 	{
 		email: 'email8@gmail.com',
 		entry: 'video',
 		title: 'title8',
-		description: 'description8',
-		link: 'http://link8.com'
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem sequi ipsum ex dolores, placeat dolor blanditiis est voluptatibus similique ad nobis ab fugit explicabo tempore asperiores aliquid id itaque neque.',
+		link: 'http://link8.com',
+		number: 7
 	},
 	{
 		email: 'email9@gmail.com',
 		entry: 'video',
 		title: 'title9',
-		description: 'description9',
-		link: 'http://link9.com'
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem sequi ipsum ex dolores, placeat dolor blanditiis est voluptatibus similique ad nobis ab fugit explicabo tempore asperiores aliquid id itaque neque.',
+		link: 'http://link9.com',
+		number: 8
 	},
 	{
 		email: 'email10@gmail.com',
 		entry: 'video',
 		title: 'title10',
-		description: 'description10',
-		link: 'http://link10.com'
-	},
-	{
-		email: 'email11@gmail.com',
-		entry: 'video',
-		title: 'title11',
-		description: 'description11',
-		link: 'http://link11.com'
-	},
-	{
-		email: 'email12@gmail.com',
-		entry: 'video',
-		title: 'title12',
-		description: 'description12',
-		link: 'http://link12.com'
-	},
-	{
-		email: 'email13@gmail.com',
-		entry: 'video',
-		title: 'title13',
-		description: 'description13',
-		link: 'http://link13.com'
-	},
-	{
-		email: 'email14@gmail.com',
-		entry: 'video',
-		title: 'title14',
-		description: 'description14',
-		link: 'http://link14.com'
-	},
-	{
-		email: 'email15@gmail.com',
-		entry: 'video',
-		title: 'title15',
-		description: 'description15',
-		link: 'http://link15.com'
-	},
-	{
-		email: 'email16@gmail.com',
-		entry: 'video',
-		title: 'title16',
-		description: 'description16',
-		link: 'http://link16.com'
-	},
-	{
-		email: 'email17@gmail.com',
-		entry: 'video',
-		title: 'title17',
-		description: 'description17',
-		link: 'http://link17.com'
-	},
-	{
-		email: 'email18@gmail.com',
-		entry: 'video',
-		title: 'title18',
-		description: 'description18',
-		link: 'http://link18.com'
-	},
-	{
-		email: 'email19@gmail.com',
-		entry: 'video',
-		title: 'title19',
-		description: 'description19',
-		link: 'http://link19.com'
-	},
-	{
-		email: 'email20@gmail.com',
-		entry: 'video',
-		title: 'title20',
-		description: 'description20',
-		link: 'http://link20.com'
-	},
-	{
-		email: 'email21@gmail.com',
-		entry: 'video',
-		title: 'title21',
-		description: 'description21',
-		link: 'http://link21.com'
+		description:
+			'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem sequi ipsum ex dolores, placeat dolor blanditiis est voluptatibus similique ad nobis ab fugit explicabo tempore asperiores aliquid id itaque neque.',
+		link: 'http://link10.com',
+		number: 9
 	}
 ];
