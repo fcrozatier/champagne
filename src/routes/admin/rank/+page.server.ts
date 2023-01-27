23a0701d71b12eb22ed3e424227d7033835bbf4e
fcrozatier <frederic.crozatier@protonmail.com>
2023-01-27 19:34:27 +0100

    use PageRank

--- src/routes/admin/rank/+page.server.ts
+++ src/routes/admin/rank/+page.server.ts
@@ -1,74 +1,28 @@
 import { driver } from '$lib/server/neo4j';
 import type { Actions } from './$types';
-import type { PageServerLoad } from './$types';
 
-export const load: PageServerLoad = async () => {
-	const session = driver.session();
-
-	try {
-		const res = await session.executeRead((tx) => {
-			return tx.run(`
-          MATCH (D:Diameter)
-          RETURN D.value as diameter
-        `);
-		});
-
-		let diameter = null;
-		if (res?.records?.length) {
-			diameter = res.records[0].get('diameter').toInt();
-		}
-
-		return { diameter };
-	} finally {
-		session.close();
-	}
-};
-
-let ID: 'diameter' | 'rank';
 export const actions: Actions = {
-	compute_diameter: async () => {
-		ID = 'diameter';
-		const session = driver.session();
-
-		try {
-			// Compute the diameter of the graph as the longest shortest path
-			// Then save the value in a Diameter Node
-			const res = await session.executeWrite((tx) => {
-				return tx.run(`
-          MATCH (n:Entry)
-          WITH collect(n) AS nodes
-          UNWIND nodes as a
-          UNWIND nodes as b
-          WITH a,b
-          WHERE id(a) < id(b)
-          MATCH path=shortestPath((a)-[*]-(b))
-          WITH length(path) AS diameter
-          ORDER BY diameter DESC
-          LIMIT 1
-					MERGE (D:Diameter)
-					SET D.value = diameter
-					RETURN diameter
-        `);
-			});
-
-			const diameter = res.records[0].get('diameter').toInt();
-			return { ID, diameter };
-		} finally {
-			session.close();
-		}
-	},
 	rank: async () => {
-		ID = 'rank';
-
 		const session = driver.session();
 
 		try {
-			// Saves the subgraph of entries and votes as 'network' for the PageRank
-			await session.executeWrite((tx) => {
+			// Check if the subgraph projection already exists
+			const projection = await session.executeRead((tx) => {
 				return tx.run(`
-          CALL gds.graph.project('network','Entry','LOSES_TO')
+          CALL gds.graph.exists('votesNetwork')
+					YIELD graphName, exists
+					RETURN exists
         `);
 			});
+
+			if (!projection.records[0].get('exists')) {
+				// Saves the subgraph of Entries and Votes as 'network' for the PageRank
+				await session.executeWrite((tx) => {
+					return tx.run(`
+          CALL gds.graph.project('network','Entry','LOSES_TO')
+					`);
+				});
+			}
 
 			// Performs the PageRank
 			const res = await session.executeWrite((tx) => {
@@ -78,16 +32,20 @@ export const actions: Actions = {
 					WITH gds.util.asNode(nodeId) as node, score
 					RETURN node.title AS title, node.link as link, score
 					ORDER BY score DESC, title ASC
-					LIMIT 2
+					LIMIT 100
         `);
 			});
 
 			const ranking = [];
 			for (const row of res.records) {
-				ranking.push({ title: row.get('title'), link: row.get('link'), score: row.get('score') });
+				ranking.push({
+					title: row.get('title') as string,
+					link: row.get('link') as string,
+					score: row.get('score') as number
+				});
 			}
 
-			return { ID, ranking };
+			return { ranking };
 		} finally {
 			session.close();
 		}
--- src/routes/admin/rank/+page.svelte
+++ src/routes/admin/rank/+page.svelte
@@ -2,7 +2,6 @@
 	import { enhance } from '$app/forms';
 	import type { ActionData, PageData } from './$types';
 
-	export let data: PageData;
 	export let form: ActionData;
 
 	let computing = false;
@@ -10,53 +9,25 @@
 
 <article class="layout-prose">
 	<h2>Rank entries</h2>
-	<h3>A. Compute the diameter</h3>
-	{#if data.diameter}
-		<p>
-			The diameter of the graph is {data.diameter}
-		</p>
-	{/if}
-	{#if !computing}
+	<p>Applies a PageRank algorithm and returns the top 100 entries</p>
+	{#if computing}
+		<p>Computing... this can take a moment</p>
+	{:else}
 		<form
 			method="post"
-			action="?/compute_diameter"
-			use:enhance={({ form }) => {
-				const button = form.querySelector('button');
-				button?.setAttribute('disabled', 'on');
+			action="?/rank"
+			use:enhance={() => {
 				computing = true;
 				return async ({ update }) => {
 					computing = false;
-					button?.removeAttribute('disabled');
 					await update();
 				};
 			}}
 		>
-			<p>Compute the diameter of the graph</p>
-			<button class="btn " type="submit">Compute</button>
+			<button class="btn" type="submit">Rank</button>
 		</form>
-	{:else}
-		<p>Processing... This can take a moment</p>
 	{/if}
-
-	<h3>B. Bubble-up the better entries</h3>
-	<p>Applies a PageRank algorithm and returns the top 100 entries</p>
-	<form
-		method="post"
-		action="?/rank"
-		use:enhance={({ form }) => {
-			const button = form.querySelector('button');
-			button?.setAttribute('disabled', 'on');
-			computing = true;
-			return async ({ update }) => {
-				computing = false;
-				button?.removeAttribute('disabled');
-				await update();
-			};
-		}}
-	>
-		<button class="btn" type="submit">Rank</button>
-	</form>
-	{#if form?.ID === 'rank'}
+	{#if form?.ranking}
 		<table class="table table-zebra w-full">
 			<thead>
 				<tr>
@@ -72,8 +43,6 @@
 						<td><a class="capitalize" href={link}>{title}</a></td>
 						<td><span>{score}</span></td>
 					</tr>
-				{:else}
-					<p>No entries ranked</p>
 				{/each}
 			</tbody>
 		</table>
