260a4fe564718277f9b425d67c3552455c2a379f
fcrozatier <frederic.crozatier@protonmail.com>
2023-02-01 10:20:15 +0100

    add error page

---
+++ src/routes/+error.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+	import { page } from '$app/stores';
+</script>
+
+<article class="layout-prose min-h-[100vh]">
+	<h2>{$page.status}</h2>
+	<p>
+		{$page.error?.message}
+	</p>
+</article>
