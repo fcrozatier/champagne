c094b5a41c305685e2c70d5fb181082cd1138eb1
fcrozatier <frederic.crozatier@protonmail.com>
2023-02-08 11:23:11 +0100

    display flag reason

--- src/routes/admin/flagged/+page.svelte
+++ src/routes/admin/flagged/+page.svelte
@@ -8,20 +8,23 @@
 	$: flagged = data.flagged as EntryProperties[];
 
 	export let form: ActionData;
+	$: flag = form?.flag;
+	$: flagError = form?.flagError;
 	$: unflag = form?.unflag;
 	$: unflagError = form?.unflagError;
 </script>
 
-<article class="mx-auto max-w-xl overflow-x-auto">
-	<h2>Flagged entries</h2>
+<article class="mx-auto w-4/5 max-w-5xl overflow-x-auto">
+	<h2>Reported entries to review</h2>
 
 	<table class="table-zebra table w-full">
 		<thead>
 			<tr>
 				<th />
 				<th>Title</th>
-				<th>Flagged by</th>
-				<th>Action</th>
+				<th>Reported by</th>
+				<th>Reason</th>
+				<th>Actions</th>
 			</tr>
 		</thead>
 		<tbody>
@@ -30,41 +33,53 @@
 					<th>{i + 1}</th>
 					<td><a class="capitalize" href={entry.link}>{entry.title}</a></td>
 					<td><span class="capitalize">{entry.flaggedBy}</span></td>
+					<td><span class="capitalize">{entry.flagReason}</span></td>
 					<td>
 						<form
+							class="flex gap-2"
 							method="post"
-							use:enhance={() => {
+							use:enhance={({ action }) => {
 								const buttons = document.querySelectorAll('button');
 								buttons.forEach((b) => b.setAttribute('disabled', 'on'));
 								return async ({ update }) => {
 									buttons.forEach((b) => b.removeAttribute('disabled'));
 									await update();
 									setTimeout(() => {
-										unflag = false;
-										unflagError = false;
+										if (action.pathname.includes('unflag')) {
+											unflag = false;
+											unflagError = false;
+										} else {
+											flag = false;
+											flagError = false;
+										}
 									}, 3000);
 								};
 							}}
 						>
 							<input type="hidden" value={entry.link} name="link" />
-							<button type="submit" class="btn-sm btn">Unflag</button>
+							<button type="submit" formaction="?/unflag" class="btn-sm btn">Unflag</button>
+							<button type="submit" formaction="?/flag" class="btn-error btn-sm btn">Flag</button>
 						</form>
 					</td>
 				</tr>
 			{:else}
-				<p>No entries flagged</p>
+				<p>No entries to review</p>
 			{/each}
 		</tbody>
 	</table>
 </article>
 
-{#if unflag}
+{#if flag || unflag}
 	<div transition:fade class="toast-center toast-bottom toast">
-		<span class="alert alert-success whitespace-nowrap"> Entry unflagged successfully. </span>
+		<span class="alert alert-success whitespace-nowrap">
+			Entry {form?.unflag ? 'un' : ''}flagged successfully.
+		</span>
 	</div>
 {/if}
-{#if unflagError}
+{#if unflagError || flagError}
 	<div transition:fade class="toast-center toast-bottom toast">
-		<span class="alert alert-error whitespace-nowrap"> Couldn't unflag entry. </span>
+		<span class="alert alert-error whitespace-nowrap">
+			Couldn't {form?.unflag ? 'un' : ''}flag entry.
+		</span>
 	</div>
 {/if}
