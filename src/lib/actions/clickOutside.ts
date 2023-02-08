7092930f91d265ada8b69d565f7c3893f93ca9a1
fcrozatier <frederic.crozatier@protonmail.com>
2023-02-08 11:15:04 +0100

    clickOutside action

---
+++ src/lib/actions/clickOutside.ts
@@ -0,0 +1,15 @@
+export function clickOutside(node: Element, callback: (node?: Element) => void) {
+	const handleClick = (e: Event) => {
+		if (node && !node.contains(e.target as Element)) {
+			callback(node);
+		}
+	};
+
+	document.addEventListener('click', handleClick, true);
+
+	return {
+		destroy() {
+			document.removeEventListener('click', handleClick, true);
+		},
+	};
+}
