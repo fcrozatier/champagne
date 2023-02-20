10d5a200d6886e5819223140863b52977ded0c70
fcrozatier <frederic.crozatier@protonmail.com>
2023-02-20 12:43:54 +0100

    navigate between categories

--- src/routes/vote/[token]/+page.svelte
+++ src/routes/vote/[token]/+page.svelte
@@ -1,1 +1,14 @@
-video or not ?
+<script lang="ts">
+	import { categories } from '$lib/categories';
+	import { page } from '$app/stores';
+</script>
+
+<article class="layout-prose">
+	<h2>Vote</h2>
+	<p>Choose a category:</p>
+	<p class="flex gap-4">
+		{#each categories as category}
+			<a class="btn" href={`/vote/${$page.params.token}/${category}`}>{category} entries</a>
+		{/each}
+	</p>
+</article>
