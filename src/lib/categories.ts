d918ac4c66cc950f8f0e9502ad1aa1a495edd654
fcrozatier <frederic.crozatier@protonmail.com>
2023-02-20 12:30:18 +0100

    remove unused doc

--- src/lib/categories.ts
+++ src/lib/categories.ts
@@ -1,6 +1,3 @@
-// Do not use spaces or dashes in category names as they are used as labels
-// Use _ instead
-// https://neo4j.com/docs/cypher-manual/current/syntax/naming/
 export const categories = ['Video', 'Non_Video'] as const;
 
 export type Category = (typeof categories)[number];
