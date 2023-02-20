eea130be9520b7b7039a14a9647f483887f362ab
fcrozatier <frederic.crozatier@protonmail.com>
2023-02-20 12:31:55 +0100

    verify token in vote layout

---
+++ src/routes/vote/[token]/+layout.server.ts
@@ -0,0 +1,40 @@
+import { MAX_AGE } from '$lib/server/config';
+import { driver } from '$lib/server/neo4j';
+import { error } from '@sveltejs/kit';
+import type { LayoutServerLoad } from './$types';
+
+export const load: LayoutServerLoad = async (event) => {
+	const { token } = event.params;
+
+	event.cookies.set('token', token, {
+		path: '/',
+		maxAge: MAX_AGE
+	});
+
+	const session = driver.session();
+
+	try {
+		// Find user
+		const user = await session.executeRead((tx) => {
+			return tx.run(
+				`
+				MATCH (u:User)
+				WHERE u.token = $token
+				RETURN u
+      `,
+				{
+					token
+				}
+			);
+		});
+
+		if (!user?.records?.length) {
+			throw error(401, 'Invalid token: you can use the link you received by email to vote');
+		}
+	} catch (error) {
+		console.log(error);
+		throw error;
+	} finally {
+		session.close();
+	}
+};
