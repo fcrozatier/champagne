910967d9f76102a496cfbe22c4fda394aaffa231
fcrozatier <frederic.crozatier@protonmail.com>
2023-02-20 14:44:39 +0100

    string utils

--- src/lib/categories.ts
+++ src/lib/categories.ts
@@ -1,3 +1,15 @@
-export const categories = ['Video', 'Non Video'] as const;
+// No space as the strings are used in vote url
+export const categories = ['video', 'non-video'] as const;
 
 export type Category = (typeof categories)[number];
+
+const capitalize = (str: string) => {
+	return str.length > 0 ? str[0].toUpperCase() + str.slice(1) : str;
+};
+
+export const display = (str: string) => {
+	return str
+		.split('-')
+		.map((s) => capitalize(s))
+		.join(' ');
+};
