1f36ed8579ed80b73b9f2018047d6e761009636f
fcrozatier <frederic.crozatier@protonmail.com>
2023-02-22 09:50:23 +0100

    add dynamic navigation

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,14 +1,16 @@
-<script>
+<script lang="ts">
 	import '../app.css';
 	import { voteOpen } from '$lib/utils';
-	import { page } from '$app/stores';
+	import type { LayoutData } from './$types';
+
+	export let data: LayoutData;
 </script>
 
 <div class="flex min-h-[100vh] flex-col">
 	<header class="navbar px-4">
 		<nav class="flex gap-4">
 			<a href="/">Home</a>
-			{#if voteOpen() && $page.url.pathname.includes('/vote')}
+			{#if voteOpen() && data.token}
 				<a href="/vote">Vote</a>
 			{/if}
 		</nav>
@@ -21,7 +23,10 @@
 		<slot />
 	</main>
 
-	<footer class="mt-auto p-4">
+	<footer class="mt-auto flex gap-4 p-4">
 		<a href="https://github.com/fcrozatier/champagne"> Champagne </a>
+		<a href="/gdpr"> GDPR </a>
+		<a href="/rules"> Rules </a>
+		<a href="/contact"> Contact </a>
 	</footer>
 </div>
