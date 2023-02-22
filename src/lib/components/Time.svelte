d6e12a1c7e3134ad09740f9c36db126151177493
fcrozatier <frederic.crozatier@protonmail.com>
2023-02-22 11:08:49 +0100

    time is optional

--- src/lib/components/Time.svelte
+++ src/lib/components/Time.svelte
@@ -1,16 +1,15 @@
-<script context="module">
+<script lang="ts">
+	export let datetime: string;
+	export let time = true;
+
 	const intl = new Intl.DateTimeFormat('en', {
 		// weekday: 'long',
 		year: 'numeric',
 		month: 'long',
 		day: 'numeric',
-		hour: 'numeric',
-		minute: 'numeric'
+		hour: time ? 'numeric' : undefined,
+		minute: time ? 'numeric' : undefined
 	});
-</script>
-
-<script lang="ts">
-	export let datetime: string;
 </script>
 
 <time {datetime}>
