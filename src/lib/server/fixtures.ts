ac99c62c94874fdafeb24c5f6d9db88e08bf37a6
fcrozatier <frederic.crozatier@protonmail.com>
2023-02-22 09:34:50 +0100

    improve fixtures

--- src/lib/server/fixtures.ts
+++ src/lib/server/fixtures.ts
@@ -1,3 +1,5 @@
+import { categories } from '../categories';
+
 export function makeCreators(n: number) {
 	const creators = [];
 
@@ -5,7 +7,7 @@ export function makeCreators(n: number) {
 		creators.push({
 			email: `email${i}@gmail.com`,
 			title: `title${i}`,
-			category: 'Video',
+			category: categories[0],
 			description:
 				'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem sequi ipsum ex dolores, placeat dolor blanditiis est voluptatibus similique ad nobis ab fugit explicabo tempore asperiores aliquid id itaque neque.',
 			link: `http://link${i}.com`,
--- src/routes/admin/fixtures/+page.server.ts
+++ src/routes/admin/fixtures/+page.server.ts
@@ -2,6 +2,7 @@ import type { PageServerLoad } from './$types';
 import { driver } from '$lib/server/neo4j';
 import { makeCreators } from '$lib/server/fixtures';
 import { dev } from '$app/environment';
+import { categories } from '$lib/categories';
 
 export const load: PageServerLoad = async () => {
 	if (!dev) return;
@@ -15,7 +16,7 @@ export const load: PageServerLoad = async () => {
 				`
 	UNWIND $creatorsData as data
 	WITH data
-  CREATE (u:User:Creator {email: data.email})-[:CREATED]->(e:Entry:Video {title: data.title, description: data.description, category: data.category, link: data.link, number: toInteger(data.number)})
+  CREATE (u:User:Creator {email: data.email})-[:CREATED]->(e:Entry {title: data.title, description: data.description, category: data.category, link: data.link, number: toInteger(data.number)})
 	SET e.points = 1, u.token = randomUUID()
   `,
 				{
@@ -26,10 +27,11 @@ export const load: PageServerLoad = async () => {
 		await session.executeWrite((tx) => {
 			return tx.run(
 				`
-	MATCH (s:Seq) WHERE s.category = "Video"
+	MATCH (s:Seq) WHERE s.category = $category
 	SET s.value = size($creatorsData)
   `,
 				{
+					category: categories[0],
 					creatorsData
 				}
 			);
