292370a93eaeb58bd583553e5ea89e73fd719301
fcrozatier <frederic.crozatier@protonmail.com>
2023-02-22 10:34:52 +0100

    contact page

---
+++ src/routes/contact/+page.svelte
@@ -0,0 +1,16 @@
+<script lang="ts">
+	import { PUBLIC_COMPETITION_NAME } from '$env/static/public';
+</script>
+
+<svelte:head>
+	<title>Contact &middot; {PUBLIC_COMPETITION_NAME}</title>
+</svelte:head>
+
+<article class="layout-prose">
+	<h2>Do you have a question?</h2>
+
+	<p>If you have any questions or suggestions, please contact us at:</p>
+	<address>
+		<a href="mailto:3b1b.some@gmail.com"> 3b1b.some@gmail.com</a>
+	</address>
+</article>
