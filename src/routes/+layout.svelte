1e6a935dc833ae647a6c7b6eaab78b56d996f74a
fcrozatier <frederic.crozatier@protonmail.com>
2023-02-22 11:38:34 +0100

    add github icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -3,13 +3,14 @@
 	import { voteOpen } from '$lib/utils';
 	import type { LayoutData } from './$types';
 	import { PUBLIC_COMPETITION_NAME } from '$env/static/public';
+	import Icon from '$lib/components/Icon.svelte';
 
 	export let data: LayoutData;
 </script>
 
 <div class="flex min-h-[100vh] flex-col">
-	<header class="navbar px-4">
-		<nav class="flex gap-4">
+	<header class="navbar px-8">
+		<nav class="flex gap-8">
 			<a href="/">Home</a>
 			{#if voteOpen() && data.token}
 				<a href="/vote">Vote</a>
@@ -24,8 +25,10 @@
 		<slot />
 	</main>
 
-	<footer class="mt-auto flex gap-4 p-4">
-		<a href="https://github.com/fcrozatier/champagne"> Champagne </a>
+	<footer class="mt-auto flex flex-wrap justify-center gap-8 p-4">
+		<a href="https://github.com/fcrozatier/champagne" class="inline-flex items-center gap-2"
+			><Icon name="github" /> Champagne
+		</a>
 		<a href="/gdpr"> GDPR </a>
 		<a href="/rules"> Rules </a>
 		<a href="/contact"> Contact </a>
