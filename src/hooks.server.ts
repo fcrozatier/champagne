fa28ffe67cbecd838753f9bcb24753297f641905
fcrozatier <frederic.crozatier@protonmail.com>
2023-03-17 10:56:59 +0100

    add hooks

---
+++ src/hooks.server.ts
@@ -0,0 +1,6 @@
+import type { Handle } from '@sveltejs/kit';
+
+export const handle = (async ({ event, resolve }) => {
+	const response = await resolve(event);
+	return response;
+}) satisfies Handle;
