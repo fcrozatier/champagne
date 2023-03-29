654d3087c3ac91ff7bf50ffec635c1c4ef58151c
fcrozatier <frederic.crozatier@protonmail.com>
2023-03-29 11:47:31 +0200

    allow serving from top level dirs

--- vite.config.js
+++ vite.config.js
@@ -1,10 +1,17 @@
 import { sveltekit } from '@sveltejs/kit/vite';
+import path from 'path';
 
 /** @type {import('vite').UserConfig} */
 const config = {
 	plugins: [sveltekit()],
 	test: {
 		include: ['src/**/*.{test,spec}.{js,ts}']
+	},
+	server: {
+		fs: {
+			// Allow serving files from one level up to the project root (src/ folder with app.html)
+			allow: [path.resolve('.')]
+		}
 	}
 };
 
