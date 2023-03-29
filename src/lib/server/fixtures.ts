496ad230deb7fbc91975fef3460758347eb00f60
fcrozatier <frederic.crozatier@protonmail.com>
2023-03-29 11:47:41 +0200

    fix fixtures

--- src/lib/server/fixtures.ts
+++ src/lib/server/fixtures.ts
@@ -4,14 +4,14 @@ export function makeCreators(n: number) {
 	const creators = [];
 
 	for (const [c, category] of categories.entries()) {
-		for (let i = c * n; i < (c + 1) * n; i++) {
+		for (let i = 0; i < n; i++) {
 			creators.push({
-				email: `email${i}@gmail.com`,
+				email: `email${c * n + i}@gmail.com`,
 				title: `title${i}`,
 				category: category,
 				description:
 					'Lorem ipsum, dolor sit amet consectetur adipisicing elit. Dolorem sequi ipsum ex dolores, placeat dolor blanditiis est voluptatibus similique ad nobis ab fugit explicabo tempore asperiores aliquid id itaque neque.',
-				link: `http://link${i}.com`,
+				link: `http://link${c * n + i}.com`,
 				number: i
 			});
 		}
