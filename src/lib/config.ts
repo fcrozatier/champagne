98328e0697b0c590a99285cbf5823eaaaaa45d75
fcrozatier <frederic.crozatier@protonmail.com>
2023-04-07 13:49:23 +0200

    refactor config

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,9 +1,10 @@
+// The competition name
+export const COMPETITION = 'SoME3';
+
+// The different possible categories for entries
 // No space as the strings are used in vote url
 export const categories = ['video', 'non-video'] as const;
 export type Category = (typeof categories)[number];
-
-// The competition name
-export const COMPETITION = 'SoME3';
 
 const capitalize = (str: string) => {
 	return str.length > 0 ? str[0].toUpperCase() + str.slice(1) : str;
