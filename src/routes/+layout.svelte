bad22f3f0a1af9857f2a2d6cad070045d2a43fc9
fcrozatier <frederic.crozatier@protonmail.com>
2023-04-19 14:20:45 +0200

    fix logo path

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -18,7 +18,7 @@
 		<nav class="flex gap-8">
 			<a class="inline-flex items-center gap-4" href="/">
 				<!-- <Icon height="3rem" width="3rem" name="logo" /> -->
-				<img src="/static/images/logo.png" width="64" height="64" alt="Logo" />
+				<img src="/images/logo.png" width="64" height="64" alt="Logo" />
 				<span>Home</span></a
 			>
 			{#if voteOpen() && data.token}
