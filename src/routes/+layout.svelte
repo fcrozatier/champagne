dc83e9b64cc1213b3585ffa2fb7a6f26181c7b32
fcrozatier <frederic.crozatier@protonmail.com>
2023-04-21 10:07:40 +0200

    add beta test banner

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -27,9 +27,12 @@
 				<a href="/admin">Admin</a>
 			{/if}
 		</nav>
-
-		<div class="flex-1" />
 	</header>
+	<div class="alert alert-error mx-auto max-w-2xl text-sm">
+		The Peer Review website is currently undergoing a beta test. If you would like to help with the
+		test, please submit your favorite 3blue1brown video or blog post as an entry and participate in
+		each phase of the competition!
+	</div>
 
 	<main class="prose mb-40 mt-8 max-w-full">
 		<h1 class="text-center">{COMPETITION}</h1>
