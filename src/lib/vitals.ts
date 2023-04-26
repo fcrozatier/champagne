85114d3ef7262d8084048968fa04352d2dfb3af8
fcrozatier <frederic.crozatier@protonmail.com>
2023-04-26 12:41:22 +0200

    vitals module

---
+++ src/lib/vitals.ts
@@ -0,0 +1,65 @@
+import type { Metric } from 'web-vitals';
+import { onCLS, onFCP, onFID, onLCP, onTTFB } from 'web-vitals';
+
+const vitalsUrl = 'https://vitals.vercel-analytics.com/v1/vitals';
+
+function getConnectionSpeed() {
+	console.log('Connection Speed', navigator?.connection?.effectiveType ?? '');
+	// eslint-disable-next-line @typescript-eslint/ban-ts-comment
+	// @ts-ignore
+	return navigator?.connection?.effectiveType ?? '';
+}
+
+type Options = {
+	params: { [s: string]: any } | ArrayLike<any>;
+	path: string;
+	analyticsId: string;
+	debug?: boolean;
+};
+
+function sendToAnalytics(metric: Metric, options: Options) {
+	const page = Object.entries(options.params).reduce(
+		(acc, [key, value]) => acc.replace(value, `[${key}]`),
+		options.path
+	);
+
+	const body = {
+		dsn: options.analyticsId, // qPgJqYH9LQX5o31Ormk8iWhCxZO
+		id: metric.id, // v2-1653884975443-1839479248192
+		page, // /blog/[slug]
+		href: location.href, // https://{my-example-app-name-here}/blog/my-test
+		event_name: metric.name, // TTFB
+		value: metric.value.toString(), // 60.20000000298023
+		speed: getConnectionSpeed() // 4g
+	};
+
+	if (options.debug) {
+		console.log('[Analytics]', metric.name, JSON.stringify(body, null, 2));
+	}
+
+	const blob = new Blob([new URLSearchParams(body).toString()], {
+		// This content type is necessary for `sendBeacon`
+		type: 'application/x-www-form-urlencoded'
+	});
+	if (navigator.sendBeacon) {
+		navigator.sendBeacon(vitalsUrl, blob);
+	} else
+		fetch(vitalsUrl, {
+			body: blob,
+			method: 'POST',
+			credentials: 'omit',
+			keepalive: true
+		});
+}
+
+export function webVitals(options: Options) {
+	try {
+		onFID((metric) => sendToAnalytics(metric, options));
+		onTTFB((metric) => sendToAnalytics(metric, options));
+		onLCP((metric) => sendToAnalytics(metric, options));
+		onCLS((metric) => sendToAnalytics(metric, options));
+		onFCP((metric) => sendToAnalytics(metric, options));
+	} catch (err) {
+		console.error('[Analytics]', err);
+	}
+}
