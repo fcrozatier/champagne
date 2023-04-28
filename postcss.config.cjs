e40e7589bcc05b6a4879ff4b6f70e9bec5f0e55f
fcrozatier <frederic.crozatier@protonmail.com>
2023-04-28 08:59:33 +0200

    configure autoprefixer

--- postcss.config.cjs
+++ postcss.config.cjs
@@ -1,6 +1,19 @@
+const tailwindcss = require('tailwindcss');
+const autoprefixer = require('autoprefixer');
+const cssnano = require('cssnano');
+
+const mode = process.env.NODE_ENV;
+const dev = mode === 'development';
+
 module.exports = {
-	plugins: {
-		tailwindcss: {},
-		autoprefixer: {}
-	}
+	plugins: [
+		tailwindcss(),
+
+		!dev && autoprefixer(),
+
+		!dev &&
+			cssnano({
+				preset: 'default'
+			})
+	]
 };
