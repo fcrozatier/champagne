eed76ef70e8b908c4a2d12a7cdea1b3acfeee729
fcrozatier <frederic.crozatier@protonmail.com>
2023-05-02 15:21:21 +0200

    export options

--- src/lib/components/Time.svelte
+++ src/lib/components/Time.svelte
@@ -1,15 +1,19 @@
 <script lang="ts">
 	export let datetime: string;
 	export let time = true;
+	export let options: Intl.DateTimeFormatOptions | undefined = undefined;
 
-	const intl = new Intl.DateTimeFormat('en', {
-		// weekday: 'long',
-		year: 'numeric',
-		month: 'long',
-		day: 'numeric',
-		hour: time ? 'numeric' : undefined,
-		minute: time ? 'numeric' : undefined
-	});
+	const intl = new Intl.DateTimeFormat(
+		'en',
+		options ?? {
+			// weekday: 'long',
+			year: 'numeric',
+			month: 'long',
+			day: 'numeric',
+			hour: time ? 'numeric' : undefined,
+			minute: time ? 'numeric' : undefined
+		}
+	);
 </script>
 
 <time {datetime}>
