96f84d307b5bd3f23a49e258e1643d30c51f5a44
fcrozatier <frederic.crozatier@protonmail.com>
2023-05-05 20:04:15 +0200

    add Discord Icon

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -26,20 +26,25 @@
 </script>
 
 <div class="flex min-h-[100vh] flex-col">
-	<header class="navbar p-4">
-		<nav class="flex gap-8">
-			<a class="inline-flex items-center gap-4" href="/">
-				<Icon class="rounded-full" name="logo" width="3.5em" />
-				<span>Home</span></a
-			>
-			{#if voteOpen() && data.token}
-				<a href="/vote">Vote</a>
-			{/if}
-			{#if $page.data.isAdmin}
-				<a href="/admin">Admin</a>
-			{/if}
-		</nav>
-	</header>
+	<nav class="navbar gap-8 p-4">
+		<a class="inline-flex items-center gap-4" href="/">
+			<Icon class="rounded-full" name="logo" width="3.5em" />
+			<span>Home</span></a
+		>
+		{#if voteOpen() && data.token}
+			<a href="/vote">Vote</a>
+		{/if}
+		{#if $page.data.isAdmin}
+			<a href="/admin">Admin</a>
+		{/if}
+
+		<span class="ml-auto mr-4 inline-block">
+			<a href="https://discord.gg/WZvZMVsXXR">
+				<Icon class=" fill-gray-800 hover:fill-[#5865f2]" name="discord" width="1.2rem" />
+			</a>
+		</span>
+	</nav>
+
 	<div class="alert alert-error mx-auto max-w-2xl text-sm">
 		The Peer Review website is currently undergoing a beta test. If you would like to help with the
 		test, please submit your favorite 3blue1brown video or blog post as an entry and participate in
