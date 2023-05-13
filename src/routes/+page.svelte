be56ad332eea91b629ec6b50299593e8452c1cfc
fcrozatier <frederic.crozatier@protonmail.com>
2023-05-13 18:00:31 +0200

    rework landing page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,5 +1,11 @@
 <script lang="ts">
-	import { registrationOpen, resultsAvailable, competitionStarted, voteOpen } from '$lib/utils';
+	import {
+		registrationOpen,
+		resultsAvailable,
+		competitionStarted,
+		voteOpen,
+		YOUTUBE_EMBEDDABLE
+	} from '$lib/utils';
 	import Time from '$lib/components/Time.svelte';
 	import type { ActionData, PageData } from './$types';
 	import { clickOutside } from '$lib/actions';
@@ -37,6 +43,14 @@
 		}
 	];
 
+	const winners = [
+		'https://explanaria.github.io/crystalgroups/',
+		'https://www.youtube.com/embed/5M2RWtD4EzI',
+		'https://www.youtube.com/embed/gsZiJeaMO48',
+		'https://www.youtube.com/embed/a-767WnbaCQ',
+		'https://www.youtube.com/embed/6hVPNONm7xw'
+	];
+
 	const dateFormat = {
 		month: 'short',
 		day: '2-digit',
@@ -64,23 +78,28 @@
 	<title>{COMPETITION_SHORT_NAME}</title>
 </svelte:head>
 
-<article class="layout-prose">
+<article class="layout-prose pb-20">
+	<p class=" mb-16 text-center text-3xl font-light">
+		A fun way to discover excellent math content, and for creators a means to be discovered.
+	</p>
 	<p>
 		The {COMPETITION_FULL_NAME} ({COMPETITION_SHORT_NAME}) is an annual competition to foster the
-		creation of excellent math content online. If you would like to register for this competition as
-		either an entrant or a judge, please use the registration button below and fill out the
-		appropriate form. Note that you may only register as an entrant during the official registration
-		phase, but you can register at a judge during the voting phase as well. If the registration
-		phase has passed, please follow the instructions on this page for either voting or receiving
-		feedback.
+		creation of excellent math content online.
 	</p>
-	<h2>Timeline</h2>
+	<p>
+		If you would like to register for this competition as either an entrant or a judge, please use
+		the registration button below and fill out the appropriate form. Note that you may only register
+		as an entrant during the official registration phase, but you can register at a judge during the
+		voting phase as well. If the registration phase has passed, please follow the instructions on
+		this page for either voting or receiving feedback.
+	</p>
+	<h2 class="mb-6 text-4xl font-black">Timeline</h2>
 	<p>The competition has three phases:</p>
-	<ul>
+	<ul class="-ml-7 list-outside">
 		{#each phases as phase, i}
-			<li class="{phase.isOpen ? 'marker:text-green-500' : ''} space-y-0">
-				<p>
-					<span class="flex items-center gap-2 font-semibold">
+			<li class={phase.isOpen ? 'marker:text-green-500' : ''}>
+				<div class="mb-4 mt-8">
+					<span class="flex items-center gap-2 text-xl font-semibold">
 						Phase {i + 1}:
 						{phase.title}
 						{#if phase.isOpen}
@@ -95,8 +114,32 @@
 							From <Time datetime={phase.dates[0]} options={dateFormat} />
 						{/if}</span
 					>
-				</p>
+				</div>
 				<p class="text-sm">{phase.description}</p>
+				{#if phase.isOpen}
+					<p class="mt-6">
+						{#if i === 0}
+							<a class="btn" href="/register"
+								>Go to registration page <span class="ml-4 inline-block">&rarr;</span></a
+							>
+						{:else if i === 1}
+							{#if data.token}
+								<a class="btn" href="/vote/{data.token}">Vote</a>
+							{:else}
+								<button
+									type="button"
+									class="btn-outline btn"
+									on:click={() => {
+										personalLinkDialog.showModal();
+									}}
+									>Resend me my personal link
+								</button>
+							{/if}
+						{:else if i === 2}
+							<a class="btn" href="/feedback/{data.token}">See feedbacks</a>
+						{/if}
+					</p>
+				{/if}
 			</li>
 		{/each}
 	</ul>
@@ -107,65 +150,59 @@
 			<p>Stay tuned for the announcement of phase 1!</p>
 		{/if}
 	{/if}
-
-	{#if registrationOpen()}
-		<h2>Register</h2>
-		<b>Phase 1 is open</b>
-		<p>
-			You can register until
-			<Time datetime={PUBLIC_REGISTRATION_END} />
-		</p>
-		<p><a class="btn" href="/register">Go to registration page</a></p>
-	{/if}
-
-	{#if voteOpen()}
-		<h2>Vote</h2>
-		<b>Phase 2 is open</b>
-		<p>
-			You can vote until
-			<Time datetime={PUBLIC_VOTE_END} />
-		</p>
-		{#if data.token}
-			<p><a class="btn" href="/vote/{data.token}">Vote</a></p>
-		{:else}
-			<p>
-				If you are registered as either an entrant or a judge, you can vote with your personal link
-				you received by email or have bookmarked after the registration.
-			</p>
-			<p>In case you can't find your personal link we can resend it to you.</p>
-			<p>
-				<button
-					type="button"
-					class="btn-outline btn"
-					on:click={() => {
-						personalLinkDialog.showModal();
-					}}
-					>Resend me my personal link
-				</button>
-			</p>
-		{/if}
-	{/if}
-
-	{#if resultsAvailable()}
-		<h2>Get your feedback</h2>
-		<p>Other people have reviewed your entry and left messages to help you improve.</p>
-		<p><a class="btn" href="/feedback/{data.token}">See feedbacks</a></p>
-	{/if}
 </article>
 
-<article id="rules" class="layout-prose">
-	<h2>Rules</h2>
+<section class="text-ligh bg-black pb-32 pt-20 text-center" style="color: var(--light-gold);">
+	<div class="mx-auto max-w-prose">
+		<h2 class="my-0 text-5xl font-black" style="color: var(--light-gold);">Last year's edition</h2>
+		<p class="mt-8 font-thin tracking-wide">
+			Discover the 5 winners of the last edition. <br />
 
-	<ol>
-		<li>It has to be something new you make this summer</li>
-		<p>
-			The spirit of this is to encourage people who've never put stuff online before. If you want to
-			work on something you sort of started once before, that's probably fine, but it can't be
-			something you already published before this contest. Optimally, you'd use this as a chance to
-			try something new you otherwise might not have.
+			The 20 honorable mentions as well as the full list of entries is available
+			<a class="font-thin text-white" href="https://www.3blue1brown.com/blog/some2">here</a>
 		</p>
+	</div>
+	<div class="carousel-center carousel rounded-box mx-auto mt-20 max-w-prose snap-always">
+		{#each winners as winner, i}
+			<div id="item-{i + 1}" class="carousel-item relative flex w-3/4 justify-center">
+				{#if YOUTUBE_EMBEDDABLE.test(winner)}
+					<iframe
+						width="480"
+						height="270"
+						class="mx-10 rounded-lg"
+						src={winner}
+						title="YouTube video player"
+						frameborder="0"
+						allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+						allowfullscreen
+					/>
+				{:else}
+					<a href={winner} target="_blank" class="cursor-pointer">
+						<iframe
+							width="480"
+							height="270"
+							class="mx-10 rounded-lg"
+							src={winner}
+							title="Winner website"
+							frameborder="0"
+							allow="accelerometer; clipboard-write; encrypted-media; gyroscope;"
+							allowfullscreen
+						/>
+					</a>
+				{/if}
+			</div>
+		{/each}
+	</div>
+</section>
 
-		<li>It has to be about math.</li>
+<section id="rules" class="layout-prose">
+	<h2 class="mb-16 text-4xl font-black">How it works</h2>
+
+	<details>
+		<summary>
+			<div class="text-xl font-medium">Is there a topic constraint?</div>
+		</summary>
+		<p>It has to be about math.</p>
 		<p>
 			Here we mean &laquo; math &raquo; very broadly, and more applied topics like physics or
 			computer science are abundantly welcome. It just has to be the case that a viewer/reader might
@@ -180,86 +217,102 @@
 			not fit into this category. So to actually win the contest, it's helpful if the topic is
 			accessible to someone with, say, a background in standard undergrad math topics.
 		</p>
-
-		<li>One entry per person/group</li>
+	</details>
+	<details>
+		<summary>
+			<div class="text-xl font-medium">How many entries can I submit?</div>
+		</summary>
+		<p>One entry per person / group</p>
 		<p>
 			We hope you make more, but we only have the capacity to judge participants based on a single
 			entry.
 		</p>
-		<li>It has to be available in English: subtitles or translation are needed</li>
+	</details>
+	<details>
+		<summary>
+			<div class="text-xl font-medium">Can I use an old entry?</div>
+		</summary>
+		<p>It has to be something new you make this summer</p>
+		<p>
+			The spirit of this is to encourage people who've never put stuff online before. If you want to
+			work on something you sort of started once before, that's probably fine, but it can't be
+			something you already published before this contest. Optimally, you'd use this as a chance to
+			try something new you otherwise might not have.
+		</p>
+	</details>
+	<details>
+		<summary>
+			<div class="text-xl font-medium">Does it has to be in english?</div>
+		</summary>
+		<p>It has to be available in English: subtitles or translation are needed.</p>
 		<p>
 			If you want to put out an explainer in another language, wonderful! Please do! But the judges
 			here will be english speakers, so to be considered for the contest the lesson has to be
 			accessible to them.
 		</p>
-		<li>It has to be publicly visible and available for people to consume for free</li>
-	</ol>
-
-	<h2>Copyright</h2>
-	<p>By registering as a creator you agree to the following copyright notice:</p>
-	<p class="indent-8 italic">
-		I have permission to use all material (music, video clips, images, software, etc.) within my
-		entry for commercial purposes. I have not used any Creative Commons Non-Commercial or
-		copyrighted work unless I have explicit permission from the copyright holder to use their
-		material. If I have used Creative Commons BY work, I have provided appropriate attribution. If I
-		have used Creative Commons Share-Alike, I have made sure my entry is also licensed under a
-		Creative Commons license.
-	</p>
-	<p>
-		Exception: We have a standing agreement with Desmos that you may use this software in the
-		competition.
-	</p>
-
-	<h2>When is the registration deadline?</h2>
-	{#if competitionStarted()}
-		<p>
-			You can register until
-			<Time datetime={PUBLIC_REGISTRATION_END} />
+	</details>
+	<details>
+		<summary>
+			<div class="text-xl font-medium">Can I use copyrighted material?</div>
+		</summary>
+		<p>By registering as a creator you agree to the following copyright notice:</p>
+		<p class="indent-8 italic">
+			I have permission to use all material (music, video clips, images, software, etc.) within my
+			entry for commercial purposes. I have not used any Creative Commons Non-Commercial or
+			copyrighted work unless I have explicit permission from the copyright holder to use their
+			material. If I have used Creative Commons BY work, I have provided appropriate attribution. If
+			I have used Creative Commons Share-Alike, I have made sure my entry is also licensed under a
+			Creative Commons license.
 		</p>
-	{:else}
-		<p>The competition has not started yet</p>
-	{/if}
-
-	<h2>How will winners be selected?</h2>
-	<p>Here's what we're looking for:</p>
-	<ul>
-		<li>
-			<b> Clarity: </b>
-			Jargon should be explained, the goals of the lesson should be understandable with minimal background,
-			and the submission should generally display empathy for people unfamiliar with the topic.
-		</li>
-		<li>
-			<b>Motivation:</b> It should be clear to the reader/viewer within the first 30 seconds why they
-			should care.
-		</li>
-		<li>
-			<b>Novelty:</b> It doesn't necessarily have to be an original idea or original topic, but it should
-			offer someone an experience they might otherwise not have by searching around online. Some of the
-			greatest value comes from covering common topics in better ways. Other times there's value in surfacing
-			otherwise obscure ideas which more people should know about.
-		</li>
-		<li>
-			<b>Memorability:</b> Something should make the piece easy to remember even several months later.
-			Maybe it's the beauty of the presentation, the enthusiasm of the presenter, or the mind-blowingness
-			of an aha moment.
-		</li>
-	</ul>
-	<p>
-		A peer review process will help filter the better entries. Then the admins will manually select
-		the best entries.
-	</p>
-	<strong>
-		If your entry does not respect the rules or copyright notice you will not be eligible to accept
-		any prize money.
-	</strong>
-	<p>
-		For longer works, judges might not be able to consume the full video/post. Again, what's hard
-		about this is that sometimes great explainers are longer, such as a full lecture and we don't
-		want to discourage those. Just understand that to select winners, there is only so much time
-		available for review, so the substance of your work should be clearly visible with a 5-10 minute
-		view.
-	</p>
-</article>
+		<p>
+			Exception: We have a standing agreement with Desmos that you may use this software in the
+			competition.
+		</p>
+	</details>
+	<details>
+		<summary>
+			<div class="text-xl font-medium">How will winners be selected?</div>
+		</summary>
+		<p>Here's what we're looking for:</p>
+		<ul>
+			<li>
+				<b> Clarity: </b>
+				Jargon should be explained, the goals of the lesson should be understandable with minimal background,
+				and the submission should generally display empathy for people unfamiliar with the topic.
+			</li>
+			<li>
+				<b>Motivation:</b> It should be clear to the reader/viewer within the first 30 seconds why they
+				should care.
+			</li>
+			<li>
+				<b>Novelty:</b> It doesn't necessarily have to be an original idea or original topic, but it
+				should offer someone an experience they might otherwise not have by searching around online.
+				Some of the greatest value comes from covering common topics in better ways. Other times there's
+				value in surfacing otherwise obscure ideas which more people should know about.
+			</li>
+			<li>
+				<b>Memorability:</b> Something should make the piece easy to remember even several months later.
+				Maybe it's the beauty of the presentation, the enthusiasm of the presenter, or the mind-blowingness
+				of an aha moment.
+			</li>
+		</ul>
+		<p>
+			A peer review process will help filter the better entries. Then the admins will manually
+			select the best entries.
+		</p>
+		<strong>
+			If your entry does not respect the rules or copyright notice you will not be eligible to
+			accept any prize money.
+		</strong>
+		<p>
+			For longer works, judges might not be able to consume the full video/post. Again, what's hard
+			about this is that sometimes great explainers are longer, such as a full lecture and we don't
+			want to discourage those. Just understand that to select winners, there is only so much time
+			available for review, so the substance of your work should be clearly visible with a 5-10
+			minute view.
+		</p>
+	</details>
+</section>
 
 <dialog class="mb-auto" bind:this={personalLinkDialog}>
 	<form
