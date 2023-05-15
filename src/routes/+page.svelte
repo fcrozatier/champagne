6fae20c23729217746895bdb12fe1ef7c7f97c4b
fcrozatier <frederic.crozatier@protonmail.com>
2023-05-15 19:10:22 +0200

    font smoothing

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -151,12 +151,12 @@
 		<h2 class="my-0 text-5xl font-black" style:color="var(--light-gold)">
 			Last year's competition
 		</h2>
-		<p class="mt-8 font-extralight tracking-wider">
+		<p class="mt-8 font-light tracking-wider">
 			Discover the 5 winners of the last edition. <br />
 
 			The 20 honorable mentions as well as the full list of entries is available
 			<a
-				class="font-extralight text-white"
+				class="font-light text-white"
 				style:color="var(--light-gold)"
 				href="https://www.3blue1brown.com/blog/some2">here</a
 			>
