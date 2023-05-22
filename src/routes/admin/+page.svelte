96cee94b3f05f3b4afea02504a74ccccbadc9b63
fcrozatier <frederic.crozatier@protonmail.com>
2023-05-22 09:09:10 +0200

    remove add constraint

--- src/routes/admin/+page.svelte
+++ src/routes/admin/+page.svelte
@@ -10,7 +10,11 @@
 	<h3>Setup</h3>
 	<ul class="space-y-4">
 		<li><a href="/admin/testdb">Test db connection</a></li>
-		<li><a href="/admin/db-setup">Add db constraints and background job</a></li>
+		<li>
+			<a href="/admin/db-setup" class="hover:cursor-not-allowed" on:click|preventDefault
+				>Add db constraints and background job</a
+			>
+		</li>
 		{#if dev}
 			<li><a href="/admin/fixtures">Add db fixtures (dev mode)</a></li>
 		{/if}
@@ -28,7 +32,7 @@
 			<li><a href="/admin/analytics">Graph analytics</a></li>
 			<li><a href="/admin/swap">Swap entries</a></li>
 			<li><a href="/admin/mailing">Send email</a></li>
-			<li><a href="/admin/rank">Rank entries</a></li>
+			<!-- <li><a href="/admin/rank">Rank entries</a></li> -->
 		{/if}
 	</ul>
 </article>
