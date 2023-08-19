66054df346a2bc903de797d1e024d2a82c251637
fcrozatier <frederic.crozatier@protonmail.com>
2023-08-19 16:51:38 +0200

    mitigate security issue

--- src/hooks.server.ts
+++ src/hooks.server.ts
@@ -1,8 +1,22 @@
-import { redirect, type Handle } from '@sveltejs/kit';
+import { redirect, type Handle, error } from '@sveltejs/kit';
 import { TokenSchema } from '$lib/server/validation';
+import { compare } from 'bcrypt';
+import { ADMIN_PASSWORD } from '$env/static/private';
 
 export const handle = async function ({ event, resolve }) {
 	const token = event.cookies.get('token');
+
+	if (event.request.url.includes('admin')) {
+		const adminPassword = event.cookies.get('password');
+		if (adminPassword) {
+			const isAdmin = await compare(ADMIN_PASSWORD, adminPassword);
+			if (!isAdmin) {
+				throw error(401, 'Not authorized');
+			}
+		} else {
+			throw error(401, 'Not authorized');
+		}
+	}
 
 	if (token) {
 		const validation = TokenSchema.safeParse(token);
