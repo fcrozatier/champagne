ce8069c73b36caccc8bc10b1af9160dd0455fddd
fcrozatier <frederic.crozatier@protonmail.com>
2023-08-25 15:54:51 +0200

    add survey form

--- src/routes/feedback/[token]/+page.server.ts
+++ src/routes/feedback/[token]/+page.server.ts
@@ -1,5 +1,8 @@
 import { driver } from '$lib/server/neo4j';
+import { fail, type Actions } from '@sveltejs/kit';
 import type { PageServerLoad } from './$types';
+import { validateForm, FeedbackSchema } from '$lib/server/validation';
+import { z } from 'zod';
 
 export const load: PageServerLoad = async (event) => {
 	const { token } = event.params;
@@ -11,9 +14,13 @@ export const load: PageServerLoad = async (event) => {
 		const res = await session.executeRead((tx) => {
 			return tx.run(
 				`
-				MATCH (u:User)-[:CREATED]->(:Entry)-[:FEEDBACK]->(f:Feedback)
+				MATCH (u:User)
 				WHERE u.token = $token
-				RETURN f.value as feedback
+				WITH u
+				OPTIONAL MATCH (u)-[:CREATED]->(:Entry)-[:FEEDBACK]->(f:Feedback)
+				WITH u, f
+				OPTIONAL MATCH (u)-[:SENT]->(s:Survey)
+				RETURN collect(f.value) as feedbacks, s IS NOT NULL as surveyTaken
       `,
 				{
 					token
@@ -21,21 +28,65 @@ export const load: PageServerLoad = async (event) => {
 			);
 		});
 
-		if (!res?.records?.length) {
-			return { noFeedback: true };
-		}
+		const feedbacks = (res?.records?.[0]?.get('feedbacks') ?? []) as string[];
+		const surveyTaken = (res?.records?.[0]?.get('surveyTaken') ?? false) as boolean;
+		console.log('surveyTaken:', surveyTaken, res.records[0]?.get('surveyTaken'));
 
-		const feedbacks: string[] = [];
-
-		for (const row of res.records) {
-			feedbacks.push(row.get('feedback') as string);
-		}
-
-		return { feedbacks };
+		return { feedbacks, surveyTaken };
 	} catch (error) {
 		console.log(error);
 		throw error;
 	} finally {
 		await session.close();
+	}
+};
+
+const SurveySchema = z.object({
+	some: z.string().pipe(z.coerce.number()),
+	site: z.string().pipe(z.coerce.number()),
+	feedback: FeedbackSchema
+});
+
+export const actions: Actions = {
+	default: async ({ request, params }) => {
+		const { token } = params;
+
+		const validation = await validateForm(request, SurveySchema);
+
+		if (!validation.success) {
+			const fieldErrors = validation.error.flatten()['fieldErrors'];
+			const reasons = Object.values(fieldErrors).flat();
+			const listFormatter = new Intl.ListFormat('en', { type: 'conjunction', style: 'short' });
+
+			return fail(400, { surveyFail: true, reason: listFormatter.format(reasons) });
+		}
+
+		const session = driver.session();
+		try {
+			await session.executeWrite((tx) => {
+				return tx.run(
+					`
+					MATCH (u:User)
+					WHERE u.token = $token
+					CREATE (u)-[:SENT]->(s:Survey)
+					SET s.some = $data.some
+					SET s.site = $data.site
+					SET s.feedback = $data.feedback
+					RETURN u
+			`,
+					{
+						token,
+						data: validation.data
+					}
+				);
+			});
+
+			return { surveySuccess: true };
+		} catch (error) {
+			console.log(error);
+			return fail(400, { surveyFail: true, reason: null });
+		} finally {
+			await session.close();
+		}
 	}
 };
--- src/routes/feedback/[token]/+page.svelte
+++ src/routes/feedback/[token]/+page.svelte
@@ -1,9 +1,21 @@
 <script lang="ts">
+	import { enhance } from '$app/forms';
 	import { PUBLIC_RESULTS_AVAILABLE } from '$env/static/public';
 	import { COMPETITION_SHORT_NAME } from '$lib/config';
-	import type { PageData } from './$types';
+	import type { ActionData, PageData } from './$types';
 
 	export let data: PageData;
+	export let form: ActionData;
+
+	let someValue = 5;
+	let siteValue = 5;
+	let feedback = '';
+
+	let errorSummary: HTMLDivElement | undefined;
+
+	$: if (form?.surveyFail) {
+		errorSummary?.scrollIntoView();
+	}
 </script>
 
 <svelte:head>
@@ -19,10 +31,140 @@
 			</p>
 		</header>
 	{/if}
-	{#if data.noFeedback}
-		<h2>Looks empty</h2>
-		<p>There's no feedback here yet, please try again later</p>
-	{:else if data.feedbacks}
+
+	<p>
+		We have been blown away by the quality of submissions this year. We hope that this competition
+		has both inspired new content creators and introduced these creators to an audience that
+		appreciates them.
+	</p>
+
+	{#if !data.surveyTaken && !form?.surveySuccess}
+		<p>
+			We recognize that the winners have not been announced, but while we are waiting on Grant,
+			James, and their panel of esteemed guest judges to finish judging, please consider filling out
+			the following form:
+		</p>
+
+		<form
+			method="post"
+			class="space-y-4"
+			use:enhance={({ submitter }) => {
+				submitter?.setAttribute('disabled', 'on');
+				return async ({ update }) => {
+					await update();
+					submitter?.removeAttribute('disabled');
+				};
+			}}
+		>
+			<div class="form-control gap-1">
+				<label for="some" class="label flex gap-2">
+					<span class="flex-1"> How satisfied are you with the Summer of Math Exposition? </span>
+				</label>
+				<input
+					id="some"
+					name="some"
+					type="range"
+					min="1"
+					max="10"
+					step="any"
+					class="range range-sm"
+					bind:value={someValue}
+					class:range-error={someValue <= 3}
+					class:range-success={someValue > 7}
+					class:range-warning={someValue > 3 && someValue <= 7}
+					required
+				/>
+				<div class="w-full flex justify-between text-xs px-1">
+					{#each Array.from({ length: 10 }) as _, i}
+						<button type="button" on:click={() => (someValue = i + 1)}>{i + 1}</button>
+					{/each}
+				</div>
+				<div class="w-full flex justify-between text-xs px-1">
+					<span>Not satisfied</span>
+
+					<span>Very satisfied</span>
+				</div>
+			</div>
+			<div class="form-control gap-1">
+				<label for="site" class="label flex gap-2">
+					<span class="flex-1">
+						How satisfied are you with the Peer Review and website this year?
+					</span>
+				</label>
+				<input
+					id="site"
+					name="site"
+					type="range"
+					min="1"
+					max="10"
+					step="any"
+					class="range range-sm"
+					bind:value={siteValue}
+					class:range-error={siteValue <= 3}
+					class:range-success={siteValue > 7}
+					class:range-warning={siteValue > 3 && siteValue <= 7}
+					required
+				/>
+				<div class="w-full flex justify-between text-xs px-1">
+					{#each Array.from({ length: 10 }) as _, i}
+						<button type="button" on:click={() => (siteValue = i + 1)}>{i + 1}</button>
+					{/each}
+				</div>
+				<div class="w-full flex justify-between text-xs px-1">
+					<span>Not satisfied</span>
+
+					<span>Very satisfied</span>
+				</div>
+			</div>
+			<div class="form-control">
+				<label for="feedback" class="label flex gap-2">
+					<span class="flex-1">
+						Do you have general feedback or ways you would like to see the Summer of Math Exposition
+						improved next year? If so, please write it here:
+					</span>
+				</label>
+				<textarea
+					name="feedback"
+					id="feedback"
+					class="textarea-bordered textarea text-base"
+					cols="50"
+					rows="10"
+					maxlength="2000"
+					bind:value={feedback}
+				></textarea>
+				<div class="label justify-end">
+					<span class="label-text-alt">{feedback.length}/2000</span>
+				</div>
+			</div>
+
+			<p>
+				<button class="btn btn-primary">Submit survey</button>
+			</p>
+			{#if form?.surveyFail}
+				<p class="text-error" bind:this={errorSummary}>
+					<span> Something went wrong. </span>
+					{#if form?.reason}
+						{form.reason}
+					{:else}
+						<span> Please try again later</span>
+					{/if}
+				</p>
+			{/if}
+		</form>
+	{:else if form?.surveySuccess}
+		<p class="text-green-600">Thank you for taking the survey!</p>
+	{/if}
+	{#if data.feedbacks.length > 0}
+		<h2>Feedbacks</h2>
+		<p>
+			During the Peer Review, we collected feedback from those who have been judging your work and
+			will be providing this feedback to you now. Please take it with a grain or two of salt. At the
+			end of the day, these are just the opinions of random people on the internet. On the other
+			hand, they are random people on the internet who decided to spend an afternoon or two
+			analyzing math content for fun. So they are probably in your target audience and it’s
+			important to hear what they have to say.
+		</p>
+		<p>With that said, here is the feedback we have associate with your entry:</p>
 		{#each data.feedbacks as feedback, i}
 			<h3>Feedback {i + 1}</h3>
 			<p class="whitespace-pre-wrap">{feedback}</p>
